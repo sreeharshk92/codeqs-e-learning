6a258edde0ca68a5ffb0f0ce7d3f5723c1d0dac5
Anjana George <anjana7581@gmail.com>
2024-10-29 03:22:04 +0530

    created course details page and userinfoform component

--- codeqs-frontend/src/App.jsx
+++ codeqs-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'; // Check this path
 import Courses from './Pages/Courses/Courses';
+import Coursedetails from './Pages/Coursedetails/Coursedetails';
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Courses" element={<Courses/>} />
+            <Route path="/Coursedetails" element={<Coursedetails/>} />
 
 
             </Routes>
